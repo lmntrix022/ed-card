47ba830791dc6474d27e2809c73745de7cfc08f0
lmntrix022 <ekq022@gmail.com>
2024-11-20 18:17:47 +0100

    8th

--- src/app/(page)/[uri]/page.js
+++ src/app/(page)/[uri]/page.js
@@ -85,7 +85,7 @@ export default async function UserPage({ params }) {
     <div className="min-h-screen flex flex-col items-center" style={{ backgroundColor: page.bgColor }}>
       {/* Bannière */}
       <div
-        className="h-36 w-full bg-cover bg-center"
+        className="h-36 w-full bg-cover bg-center rounded-lg"
         style={
           page.bgType === "color"
             ? { backgroundColor: page.bgColor }
@@ -94,9 +94,9 @@ export default async function UserPage({ params }) {
       ></div>
 
       {/* Avatar */}
-      <div className="relative -top-16 w-36 h-36 rounded-full shadow-lg overflow-hidden border-4 border-white ">
+      <div className="relative -top-20 w-36 h-36 rounded-full shadow-lg overflow-hidden border-4 border-white ">
         <Image
-          className="object-cover w-full h-full"
+          className="object-cover w-full h-full shadow-md"
           src={user.image}
           alt="avatar"
           width={256}
@@ -105,14 +105,14 @@ export default async function UserPage({ params }) {
       </div>
 
       {/* Nom et localisation */}
-      <h2 className="-mt-6 text-3xl font-semibold text-white px-4 mb-4">{page.displayName}</h2>
-      <h3 className="text-lg text-white/70 flex items-center gap-2">
+      <h2 className="-mt-6 text-3xl font-semibold text-black px-4 mb-4 ">{page.displayName}</h2>
+      <h3 className="text-lg text-black/70 flex items-center gap-2">
         <FontAwesomeIcon icon={faLocationDot} className="h-5" />
         <span>{page.location}</span>
       </h3>
 
       {/* Biographie */}
-      <p className="text-center text-white/80 max-w-lg mt-4 px-4 text-justify">{page.bio}</p>
+      <p className="text-center text-black/80 max-w-lg mt-4 px-4 text-justify">{page.bio}</p>
 
       {/* Boutons */}
       <div className="flex gap-4 justify-center mt-6 pb-4 flex-wrap ">
@@ -120,9 +120,9 @@ export default async function UserPage({ params }) {
           <Link
             key={buttonKey}
             href={buttonLink(buttonKey, page.buttons[buttonKey])}
-            className="rounded-full bg-white text-blue-950 p-3 flex items-center justify-center shadow-md shadow-neumorphism hover:shadow-neumorphism-hover active:shadow-neumorphism-active transition"
+            className="rounded-full bg-white text-blue-950 p-3 flex items-center justify-center shadow-md hover:bg-blue-950 hover:text-white transition"
           >
-            <FontAwesomeIcon className="w-6 h-6" icon={buttonsIcons[buttonKey]} />
+            <FontAwesomeIcon className="w-8 h-8" icon={buttonsIcons[buttonKey]} />
           </Link>
         ))}
       </div>
@@ -131,21 +131,20 @@ export default async function UserPage({ params }) {
       <div className="mt-6">
         <Link
           href={`/api/vcard?${vcardParams.toString()}`}
-          className="px-6 py-3 rounded-full shadow-neumorphism hover:shadow-neumorphism-hover active:shadow-neumorphism-active transition"
+          className="px-6 py-3 rounded-full bg-blue-950 text-white shadow-lg hover:bg-blue-700 transition"
         >
           Sauvegarder contact
         </Link>
       </div>
 
-
       {/* Liens */}
-      <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-6 p-6 w-full max-w-4xl">
+      <div className="mt-8 w-full max-w-4xl grid grid-cols-1 md:grid-cols-2 gap-6 p-6">
         {page.links.map((link) => (
           <Link
             key={link.url}
             href={link.url}
             target="_blank"
-            className="flex items-center gap-4 text-white p-4 rounded-lg shadow-lg shadow-neumorphism hover:shadow-neumorphism-hover active:shadow-neumorphism-active transition"
+            className="flex items-center gap-4 text-black p-4 rounded-lg shadow-lg hover:bg-blue-950 hover:text-white transition"
           >
             <div className="w-12 h-12 bg-blue-700/60 flex items-center justify-center rounded-full">
               {link.icon ? (
@@ -160,14 +159,13 @@ export default async function UserPage({ params }) {
                 <FontAwesomeIcon icon={faLink} className="w-6 h-6 text-white" />
               )}
             </div>
-            <div>
-              <h3 className="font-semibold">{link.title}</h3>
+            <div className="">
+              <h3 className="font-semibold ">{link.title}</h3>
               <p className="text-sm text-white/70">{link.subtitle}</p>
             </div>
           </Link>
         ))}
       </div>
-
     </div>
   );
 }
